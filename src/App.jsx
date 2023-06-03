2d8165228af1be404d60c6eb352c456dd413686c
Rasik Abdullah <rasikabdullah@gmail.com>
2023-06-03 14:38:08 +0600

    files addded

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const handleAddUser = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name, email};
+    console.log(user);
+
+    form.reset();
+    
+
+  }
 
   return (
     <>
       <h1>Simple CRUD</h1>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' />
+        <br /> <br />
+        <input type="email" name="email" id="" />
+        <br /> <br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   )
 }
