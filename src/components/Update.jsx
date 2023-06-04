b1b7960af3d78d6920dc215c91fa8243f6c3c293
Rasik Abdullah <rasikabdullah@gmail.com>
2023-06-04 17:06:31 +0600

    updated to db

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Update = () => {
     const loadedUser = useLoaderData();
+    console.log(loadedUser)
 
     const handleUpdate = event => {
         event.preventDefault();
         const form = event.target;
         const name = form.name.value;
         const email = form.email.value;
-        const user = {name, email};
-        console.log(user)
+        const updatedUser = { name, email };
+        console.log(updatedUser);
+
+        fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedUser)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
     }
     return (
         <div>
+            <Link to="/"><button>Home</button></Link>
             <h3>{loadedUser.name}</h3>
             <form onSubmit={handleUpdate}>
-                <input type="text" name="name" defaultValue={loadedUser?.name} id="" /> 
+                <input type="text" name="name" defaultValue={loadedUser?.name} id="" />
                 <br /> <br />
                 <input type="email" name="email" defaultValue={loadedUser?.email} id="" />
                 <br /> <br />
